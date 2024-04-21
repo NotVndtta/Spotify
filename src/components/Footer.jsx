ad94484db43a995fef932eff833c7e72f05ed4e6
NotVndtta <thenotalexey192@gmail.com>
2024-04-21 23:07:55 +0300

    Layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#181818]'>
+      Footer
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import Sidebar from './Sidebar'
+import Navbar from './Navbar'
+import Body from './Body'
+import Footer from './Footer'
 const Spotify = () => {
   return (
-    <div>
-      Spotify
+    <div className='max-w-screen max-h-screen grid overflow-hidden grid-rows-[85vh,15vh]'>
+      <div className="grid grid-cols-custom bg-[rgb(32,87,100)] bg-gradient-to-b from-transparent to-black">
+      <Sidebar />
+      <div className='h-screen w-screen overflow-auto your-scrollbar'>
+      <Navbar />
+      </div>
+      <Body />
+      </div>
+      <Footer />
     </div>
   )
 }
